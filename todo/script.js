3a3a2689ee74b0acea84cd2e2a55e0828fe2deca
iva <ivabelyh@gmail.com>
2023-10-19 01:26:09 +0500

    ToDoApp svelte

--- todo/script.js
+++ todo/script.js
@@ -2,33 +2,79 @@ const list = document.querySelector('.list-self')
 const intEl = document.querySelector('.top-int')
 const btnEl = document.querySelector('.top-btn')
 
-btnEl.addEventListener('click', () => {
-    if (intEl.value.length){
-        const divEl = document.createElement('div')
-        divEl.classList.add('list-item')
-    
-        const checkbox = document.createElement('input')
-        checkbox.type = 'checkbox'
-        checkbox.addEventListener('click', () => {
-            divEl.classList.add('list-item_done')
-        })
-    
-        const text = document.createElement('p')
-        text.innerText = intEl.value
-        text.classList.add('text')
+let itemId = 3
 
-        const img = document.createElement('img')
-        img.src = 'trash.png'
-        img.classList.add('trash')
-        img.addEventListener('click', () => {
-            divEl.remove()
-        })
-        divEl.appendChild(checkbox)
-        divEl.appendChild(text)
-        divEl.appendChild(img)
-    
-        list.appendChild(divEl)
-    
-        intEl.value = ''    
+const listItems = [
+    {
+        id: 1,
+        text: 'Text',
+        done: false
+    },
+
+    {
+        id: 2,
+        text: 'Text2',
+        done: true
     }
+]
+
+function render() {
+    list.innerHTML = ''
+    for(let item of listItems){
+        const elem = createHtmlEl(item)
+        list.appendChild(elem)
+    }
+
+}
+
+function createHtmlEl(item) {
+    const divEl = document.createElement('div')
+    divEl.classList.add('list-item')
+    
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    if(item.done){
+        checkbox.checked = true
+        divEl.classList.add('list-item_done')
+    }
+    checkbox.addEventListener('change', () => {
+        const searchItem = listItems.find(i=>i.id === item.id)
+        searchItem.done = !searchItem.done
+        render()
+    })
+    
+    const text = document.createElement('p')
+    text.innerText = item.text
+    text.classList.add('text')
+
+    const img = document.createElement('img')
+    img.src = 'trash.png'
+    img.classList.add('trash')
+    img.addEventListener('click', () => {
+        const idx = listItems.findIndex(i => i.id === item.id)
+        listItems.splice(idx, 1)
+        render()
+    })
+
+    divEl.appendChild(checkbox)
+    divEl.appendChild(text)
+    divEl.appendChild(img)
+        
+    intEl.value = ''  
+        
+    return divEl
+}
+
+btnEl.addEventListener('click', () => {
+    if(intEl.value){
+        const object = {
+            id: itemId++,
+            text: intEl.value,
+            done: false
+        }
+        listItems.push(object)
+    }
+    render()
 })
+
+render()
